0b7ab22773dc3f14489a2f0de813a981c4544b38
Jaiden Popowski <jpopowski19@gmail.com>
2024-02-24 19:43:17 +1100

    Create load_coaches.R

---
+++ load_coaches.R
@@ -0,0 +1,5 @@
+# Load AFL Coaching Stats from AFL Tables
+
+source("https://raw.githubusercontent.com/jaidenpopowski/AFL_Functions/main/fetch_coaches_games.R")
+
+coach_games <- fetch_coaches_games()
